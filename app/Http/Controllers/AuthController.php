52e9001f99d879a68f56e76205dd5f62ed163cc5
liem.phamngoc <liem.phamngoc@di4l.vn>
2024-10-03 11:33:51 +0700

    AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,29 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            "username" => "required",
+            "password" => "required|confirmed",
+            "enterprise_id" => "",
+            "permission" => "required",
+            "account_status" => "required"
+        ]);
+        $newaccounts = Accounts::create([
+
+            'password' => bcrypt($fields['password']),
+            'permission'=> $fields['permission'],
+            'account_status'=> $fields['account_status']
+        ]);
+
+        $token = $newaccounts->createToken('')->plainTextToken;
+        $response = [
+            "data" => $newaccounts,
+            'token'=> $token
+        ];
+        return response()->json($response, 201);
+    }
     public function login(Request $request)
     {
         if (!Auth::attempt($request->only('username', 'password'))) {
